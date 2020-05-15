443a2c96d611a2b9cd2bd89b4e96b777bd70524c
jasmine <youjingqiang@vip.qq.com>
2020-05-15 11:57:31 +0800

    显示当前查询的数据库

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -35,15 +35,14 @@ class ServiceProvider extends LaravelServiceProvider
             $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
 
             $bindings = $query->connection->prepareBindings($query->bindings);
-            $pdo = $query->connection->getPdo();
-            $realSql = $sqlWithPlaceholders;
+            $pdo      = $query->connection->getPdo();
+            $realSql  = $sqlWithPlaceholders;
             $duration = $this->formatDuration($query->time / 1000);
 
             if (count($bindings) > 0) {
                 $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
             }
-
-            Log::debug(sprintf('[%s] %s | %s: %s', $duration, $realSql, request()->method(), request()->getRequestUri()));
+            Log::debug(sprintf('[%s] [%s] %s | %s: %s', $query->connection->getDatabaseName(), $duration, $realSql, request()->method(), request()->getRequestUri()));
         });
     }
 
@@ -64,11 +63,11 @@ class ServiceProvider extends LaravelServiceProvider
     private function formatDuration($seconds)
     {
         if ($seconds < 0.001) {
-            return round($seconds * 1000000).'μs';
+            return round($seconds * 1000000) . 'μs';
         } elseif ($seconds < 1) {
-            return round($seconds * 1000, 2).'ms';
+            return round($seconds * 1000, 2) . 'ms';
         }
 
-        return round($seconds, 2).'s';
+        return round($seconds, 2) . 's';
     }
 }
