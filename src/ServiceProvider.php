798c2fab5e4fd1e8f880959d0a21e61d72898a5f
jasmine <youjingqiang@vip.qq.com>
2020-05-15 12:02:08 +0800

    显示当前查询的数据库

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -35,8 +35,8 @@ class ServiceProvider extends LaravelServiceProvider
             $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
 
             $bindings = $query->connection->prepareBindings($query->bindings);
-            $pdo      = $query->connection->getPdo();
-            $realSql  = $sqlWithPlaceholders;
+            $pdo = $query->connection->getPdo();
+            $realSql = $sqlWithPlaceholders;
             $duration = $this->formatDuration($query->time / 1000);
 
             if (count($bindings) > 0) {
@@ -63,11 +63,11 @@ class ServiceProvider extends LaravelServiceProvider
     private function formatDuration($seconds)
     {
         if ($seconds < 0.001) {
-            return round($seconds * 1000000) . 'μs';
+            return round($seconds * 1000000).'μs';
         } elseif ($seconds < 1) {
-            return round($seconds * 1000, 2) . 'ms';
+            return round($seconds * 1000, 2).'ms';
         }
 
-        return round($seconds, 2) . 's';
+        return round($seconds, 2).'s';
     }
 }
