b5d24b908af63b84372cd8391bc1e4b8026933b2
塵世不再 <asundust@qq.com>
2026-08-07 14:08:56 +0800

    fix: use Laravel raw SQL interpolation for query logs

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -40,19 +40,9 @@ class ServiceProvider extends LaravelServiceProvider
                 return;
             }
 
-            $sqlWithPlaceholders = str_replace(['%', '?', '%s%s'], ['%%', '%s', '?'], $query->sql);
-
-            $bindings = $query->connection->prepareBindings($query->bindings);
-            $pdo = $query->connection->getPdo();
-            $realSql = $sqlWithPlaceholders;
+            $realSql = $query->toRawSql();
             $duration = $this->formatDuration($query->time / 1000);
 
-            if (count($bindings) > 0) {
-                $realSql = vsprintf($sqlWithPlaceholders, array_map(
-                    static fn ($binding) => $binding === null ? 'NULL' : $pdo->quote($binding),
-                    $bindings
-                ));
-            }
             Log::channel(config('logging.query.channel', config('logging.default')))
                 ->debug(sprintf('[%s] [%s] %s | %s: %s', $query->connection->getDatabaseName(), $duration, $realSql,
                     request()->method(), request()->getRequestUri()));
